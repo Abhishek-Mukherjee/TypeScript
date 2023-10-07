6c7e8c9f0d5f9d4fe6d7f71748032a0400ee3b46
Abhishek <abhishekmukherjee157@gmail.com>
2023-10-08 00:01:18 +0530

    Pushing normal programs

---
+++ variablePractise.js
@@ -0,0 +1,6 @@
+"use strict";
+let firstName;
+firstName = "Abhishek Mukherjee";
+let greetings = "Good morning";
+console.log(firstName + " " + greetings);
+console.log(`${firstName} ${greetings} ....... byeee..............`);
---
+++ variablePractise.ts
@@ -0,0 +1,7 @@
+let firstName : String;
+firstName = "Abhishek Mukherjee";
+let greetings : String = "Good morning";
+console.log(firstName+" "+greetings);
+
+
+console.log(`${firstName} ${greetings} ....... byeee..............`);
